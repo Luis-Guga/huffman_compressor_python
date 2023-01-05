e95d18b05dbd3242510b78b0b59a70b4aa51788c
Luis Hartmann <luis.lauermann.hartmann@gmail.com>
2023-01-05 16:40:54 +0100

    feat: added command-line mutual exclusive args, restructured part of the main code // chore: added known_bugs file

--- modules/huffman.py
+++ modules/huffman.py
@@ -1,13 +1,19 @@
 from .classes.node import Node
+from tabulate import tabulate
+import csv
+
+
+# pseudo-end-of-file to add to the compressed file.
+PSEUDO_EOF: str = chr(255)
 
 
 def create_tree(tree: list()):
     while len(tree) > 1:
-        # get the two least frequent symbols of the list to build the tree of the tree
+        # get the two least frequent symbols of the list to build the tree
         (sym1, freq1) = tree[-1]
         (sym2, freq2) = tree[-2]
 
-        # we also remove them after the aquisition because we will turn the symbols into nodes
+        # remove them after the acquisition because we will turn the symbols into nodes
         tree = tree[:-2]
 
         # build the node of the tree
@@ -22,20 +28,174 @@ def create_tree(tree: list()):
     return tree
 
 
-def get_encoded_message(node: Node, left: bool() = False, encoding: str() = ''):
+def get_encoded_dict(node: Node, left: bool() = False, encoding: str() = ''):
     if type(node) is str:
         return {node: encoding}
 
     (l, r) = node.children()
     encoded = dict()
-    encoded.update(get_encoded_message(l[0], True, encoding + '0'))
-    encoded.update(get_encoded_message(r[0], False, encoding + '1'))
+    encoded.update(get_encoded_dict(l[0], True, encoding + '0'))
+    encoded.update(get_encoded_dict(r[0], False, encoding + '1'))
 
     return encoded
 
 
-def print_encoding(tree: Node, symbol_frequency: list()):
-    huffman_dict: dict() = get_encoded_message(tree)
+def str_to_bytes(text: str):
+    byte_list = list()
 
+    # must count how many bits we have for an integer
+    bit_count = 0
+
+    # current byte being built
+    byte = 0
+
+    # by convention, the first char of a 8-char group will be on the left
+    for ch in text:
+        # at the end of the byte, reset all the auxiliar variables and append the byte to the list
+        if bit_count == 8:
+            byte_list.append(byte)
+            bit_count = 0
+            byte = 0
+
+        # build the currenct byte by shifting the 1's to the left, because the conversion from str to integer is needed
+        # example: 0010110001 would be converted to 44 and 64 (not 2). the most important is to have the prefix correctly
+        byte |= int(ch) << (7-bit_count)
+        bit_count += 1
+
+    byte_list.append(byte)
+
+    return byte_list
+
+
+def print_encoding(tree: Node, symbol_frequency: list()):
+    # get tree encoding dictionary
+    huffman_dict: dict() = get_encoded_dict(tree)
+
+    table = list()
     for (char, freq) in symbol_frequency:
-        print('%r->%s' % (char, huffman_dict[char]))
+        table.append([char, freq, huffman_dict[char]])
+
+    print(tabulate(tabular_data=[['Encoding scheme: ']],
+                   tablefmt='fancy_outline'))
+    headers = ['CHAR', 'OCCURENCES', 'ENCODING']
+    print(tabulate(table, headers, tablefmt='fancy_outline'))
+
+
+def save_encoding(tree: Node, symbol_frequency: list(), file: str):
+    huffman_dict: dict() = get_encoded_dict(tree)
+
+    with open(file, 'w') as out_table:
+        csv_writer = csv.writer(out_table)
+        csv_writer.writerow(['CHAR', 'OCCURENCES', 'ENCODING'])
+        for (char, freq) in symbol_frequency:
+            csv_writer.writerow([char, freq, huffman_dict[char]])
+
+
+def save_binary(string: str, file: str):
+    with open(file, 'w') as out_bin:
+        out_bin.write(string)
+
+
+def print_statistics(message: str, encoded: str):
+    print("========= STATISTICS =========")
+    print("Original text size: %d bits" % (len(message)*8))
+    print("Encoded text size: %d bits" % (len(encoded)))
+    print("Size reduction: %d%%" % (100 - 100*len(encoded)/(len(message)*8)))
+    print("==============================")
+
+
+def build_header(symbol_frequency: list):
+    table = str()
+    for (char, freq) in symbol_frequency:
+        table += char + str(freq)
+    return table
+
+
+def parse_compressed_file(file: str):
+    symbol_frequency = dict()
+
+    with open(file, 'r') as input_file:
+        text: str = input_file.read()
+        header, text = pop_header(text)
+        symbol_frequency: dict = build_table_from_header(header)
+
+    return [symbol_frequency, text]
+
+
+def pop_header(text: str):
+    header = str()
+    for i in range(len(text)):
+        if text[i] == PSEUDO_EOF:
+            header += text[i] + text[i+1]
+            break
+        header += text[i]
+    text = text[len(header):]
+    return [header, text]
+
+
+def build_table_from_header(header: str):
+    symbol_frequency = dict()
+    occurences = str()
+    symbol = str()
+    got_occurences = False
+    got_symbol = False
+    for char in header:
+        if not got_symbol and not char.isdecimal():
+            symbol += char
+        elif not got_occurences and char.isdecimal():
+            got_symbol = True
+            occurences += char
+        else:
+            got_symbol = False
+            got_occurences = False
+            symbol_frequency[symbol] = int(occurences)
+            occurences = str()
+            symbol = char
+    symbol_frequency[symbol] = int(occurences)
+    return symbol_frequency
+
+
+def parse_uncompressed_file(file: str):
+    with open(file, 'r') as input_file:
+        # read everything at once. This way there are less function calls in comparison to as if one would read line by line
+        message: str = input_file.read()
+
+        message += PSEUDO_EOF
+        symbol_frequency: dict = get_frequency_table(message)
+
+    return (symbol_frequency, message)
+
+
+def get_frequency_table(text: str):
+    symbol_frequency = dict()
+    # iterate through the message in the provided file (or as input on command-line) to count the most frequent symbols
+    for symbol in text:
+
+        # add the symbols and its frequency to the list, so that we can access them later to create the huffman's tree
+        if symbol in symbol_frequency:
+            symbol_frequency[symbol] += 1
+        else:
+            symbol_frequency[symbol]: int = 1
+
+    return symbol_frequency
+
+
+def get_encoded_text(origin_text: str, huffman_dict: dict):
+    encoded_text = str()
+    for char in origin_text:
+        encoded_text += huffman_dict[char]
+
+    return encoded_text
+
+
+def write_encoded_text_to_file(header: str, encoded_text: str, file: str):
+    with open(file, 'w') as output_file:
+        # write the encoding table
+        output_file.write(header)
+
+        # convert every 32 chars into a 32 bit integer, the function returns a list of 32 bit integers
+        byte_list: list = str_to_bytes(encoded_text)
+        for byte in byte_list:
+            output_file.write(chr(byte))
+            print(chr(byte), end='')
+        print()
--- project.py
+++ project.py
@@ -24,54 +24,94 @@ import modules.huffman as huffman
 
 
 def main():
-    # set the program arguments, so that we can check the user's input
+    # set the program arguments, so that the user's input can be checked
     argparser: argparse.ArgumentParser = define_program_args()
 
     # get the arguments already parsed to analyse them
     args: argparse.Namespace = argparser.parse_args()
 
-    # check if the user provided input and output files
-    if args_complete(args):
+    # check if the user provided input and output files and if the operation of compression or decompression was given
+    if args_incomplete(args):
         argparser.print_usage()
         sys.exit(argparser.prog + ": too few comamnd-line arguments")
 
-    # initialize the variable as a dictinary, because the insertion gets easier
-    symbol_frequency = dict()
+    if args_mutex(args):
+        argparser.print_usage()
+        sys.exit(argparser.prog +
+                 ": error: arguments -o/--output: not allowed woth argument -m/--message")
+
     try:
-        with open(args.file, 'r') as input_file:
-            # read everything at once. This way there are less function calls in comparison to as if one would read line by line
-            message: str = input_file.read()
-
-            # iterate through the message in the provided file (or as input on command-line) to count the most frequent symbols
-            for symbol in message:
-
-                # add the symbols and its frequency to the list, so that we can access them later to create the huffman's tree
-                if symbol in symbol_frequency:
-                    symbol_frequency[symbol] += 1
-                else:
-                    symbol_frequency[symbol] = 1
+        if args.file:
+            if args.compress:
+                # get the symbols frequency and the file text. the text is necessary for the compression
+                symbol_frequency, text = huffman.parse_uncompressed_file(
+                    args.file)
+            else:
+                # get the symbols frequency and the file text. the text does not include the header, it was removed from the original compressed text
+                symbol_frequency, text = huffman.parse_compressed_file(
+                    args.file)
+        else:
+            text = args.message
+            symbol_frequency = huffman.get_frequency_table(text)
 
     except FileNotFoundError:
-        sys.exit(argparser.prog + ": input file does not exist")
+        sys.exit(argparser.prog + ": file does not exist")
 
-    # sort the list to ease the transformation of the list in the huffman's tree
+    # check for one or zero because a pseudo EOF was appended to the file text while counting the characters occurence, it is not possible to have a
+    # single character in a compressed file, because the table size that is written to the file has a minimum of 3 characters: <char, char_freq, pseudo-eof>
+    if len(symbol_frequency) < 2:
+        sys.exit(argparser.prog + ": input file is empty")
+
+    # sort the list by occurence frequency to ease the transformation of the list in the huffman's tree
     symbol_frequency = sorted(symbol_frequency.items(),
                               key=lambda l: l[1], reverse=True)
 
-    # copy the list. it will later repesent the tree
-    tree = symbol_frequency
+    # copy the list. it will repesent the tree as a unique element of the list
+    tree: list = symbol_frequency
 
-    # create the huffman tree. each node contains the sum of the occurrence frequency of its two children (it the node is not a leaf)
+    # create the huffman tree. each node contains the sum of the occurrence frequency of its two children (if the node is not a leaf)
     tree = huffman.create_tree(tree)
 
-    # interpretate the tree and assign '0' to the left child node and '1' to the right child node of each node (if the node is not a leaf)
-    huffman_dict: dict() = huffman.get_encoded_message(tree[0][0])
+    # interpret the tree and assign '0' to the left child node and '1' to the right child node of each node (if the node is not a leaf)
+    huffman_dict: dict() = huffman.get_encoded_dict(tree[0][0])
 
-    huffman.print_encoding(tree[0][0], symbol_frequency)
+    if args.compress:
+        header = huffman.build_header(symbol_frequency)
+
+        # need to encode the text according to each character of the original text
+        encoded_text = huffman.get_encoded_text(text, huffman_dict)
+        print("Text written")
+        if args.output:
+            huffman.write_encoded_text_to_file(
+                header, encoded_text, args.output)
+    else:
+        # decompress things, but first see if the content is identical to the written content
+        print("Text read:")
+        print(text)
+
+    if args.verbose:
+        if args.compress:
+            huffman.print_statistics(text, encoded_text)
+
+        huffman.print_encoding(tree[0][0], symbol_frequency)
+
+    if args.save_encoding_table:
+        TABLE_FILE = "huffman_encoding_table.csv"
+        print("The encoding table will be saved in: " + TABLE_FILE)
+        huffman.save_encoding(tree[0][0], symbol_frequency, TABLE_FILE)
+
+    if args.save_encoded_binary:
+        ENC_FILE = "huffman_encoded_binary.txt"
+        print("The encoding table will be saved in: " + ENC_FILE)
+        huffman.save_binary(encoded_text, ENC_FILE)
 
 
-def args_complete(args: argparse.Namespace):
-    return not args.file and not args.message
+def args_incomplete(args: argparse.Namespace):
+    return (not args.file and not args.message) or (not args.compress and not args.decompress) or (not args.message and not args.output)
+
+
+def args_mutex(args: argparse.Namespace):
+    return args.output and args.message
 
 
 def define_program_args():
@@ -80,16 +120,31 @@ def define_program_args():
         description="Compresses data from a given string in the command-line or from a file\
             using the Huffman's compression algorithm"
     )
+    mutex_group = argparser.add_mutually_exclusive_group()
+    mutex_group.add_argument(
+        "-c", "--compress", help="compress file", action='store_true')
 
-    argparser.add_argument(
+    mutex_group.add_argument("-d", "--decompress",
+                             help="decompress file", action='store_true')
+
+    mutex_group2 = argparser.add_mutually_exclusive_group()
+    mutex_group2.add_argument(
         "-f", "--file", help="file to be compressed", type=str)
 
-    argparser.add_argument(
-        "-o", "--output", help="compressor's output file (default: terminal)", default=sys.stdout,
-        type=str)
-
-    argparser.add_argument("-m", "--message", help="input a smaller message\
+    mutex_group2.add_argument("-m", "--message", help="input a smaller text\
         to see how it would be compressed.", type=str)
+
+    argparser.add_argument(
+        "-o", "--output", help="compressor's output file (default: terminal)", type=str)
+
+    argparser.add_argument("-v", "--verbose", help="show encoding table and \
+        processing texts", action='store_true')
+
+    argparser.add_argument("-t", "--save-encoding-table", help="save encoding scheme \
+        into a csv file", action='store_true')
+
+    argparser.add_argument("-s", "--save-encoded-binary", help="save the text encoded \
+        in binary before converting it to UTF-8 code", action='store_true')
 
     return argparser
 
